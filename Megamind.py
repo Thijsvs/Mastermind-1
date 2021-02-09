f0300cceb614bcb801e2d7301c5dd8f85f5e3a49
DennusBes <74312356+DennusBes@users.noreply.github.com>
2021-02-09 18:25:37 +0100

    Update Megamind.py

--- Megamind.py
+++ Megamind.py
@@ -57,118 +57,11 @@ def pegs(guess, Secretcode,algo_choice):
     for i in range(len(Secretcode)):
         if guess[i] == Secretcode[i]:
             red += 1
-            continue
-        if guess[i] in Secretcode and guess[i] != Secretcode[i]:
+
+        if guess[i] in Secretcode:
             white += 1
+    white = white-red
     return [red, white]
-
-def pegstest(guess, Secretcode,algo_choice):
-    red = 0
-    white = 0
-    tempcodecopy = Secretcode.copy()
-    guesscopy = guess.copy()
-    for i in range(0,len(tempcodecopy)):
-        if guesscopy[i] == tempcodecopy[i]:
-            red += 1
-            tempcodecopy.remove(i)
-            guesscopy.remove(i)
-
-    for i in range(0,len(tempcodecopy)):
-        if guesscopy[i] in tempcodecopy and guesscopy[i] != tempcodecopy[i]:
-            white += 1
-            tempcodecopy.remove(i)
-            guesscopy.remove(i)
-
-    return [red, white]
-
-# sends every BC to its index in HMList
-def Adjustment(BC1):
-    if BC1 == [0, 0]:
-        return 0
-    elif BC1 == [0, 1]:
-        return 1
-    elif BC1 == [0, 2]:
-        return 2
-    elif BC1 == [0, 3]:
-        return 3
-    elif BC1 == [0, 4]:
-        return 4
-    elif BC1 == [1, 0]:
-        return 5
-    elif BC1 == [1, 1]:
-        return 6
-    elif BC1 == [1, 2]:
-        return 7
-    elif BC1 == [1, 3]:
-        return 8
-    elif BC1 == [2, 0]:
-        return 9
-    elif BC1 == [2, 1]:
-        return 10
-    elif BC1 == [2, 2]:
-        return 11
-    elif BC1 == [3, 0]:
-        return 12
-    elif BC1 == [4, 0]:
-        return 13
-
-# returns positive's list minimum without including zeros
-def MinimumNozeros(List1):
-    minimum = max(List1) + 1
-    for item in List1:
-        if item != 0 and item < minimum:
-            minimum = item
-    return minimum
-
-def knutz(Secretcode):
-        for secret in Secretcode:
-            guess=[0,0,1,1]
-            BC=HowManyBc(guess[:],secret[:])
-            counter=1
-            optionList=[]
-            allList=[]
-            for i0 in range(0,6):
-                for i1 in range(0,6):
-                    for i2 in range(0,6):
-                        for i3 in range(0,6):
-                            optionList.append([i0,i1,i2,i3])
-                            allList.append([i0,i1,i2,i3])
-            #while BC!=[4,0]:
-            while len(optionList)>1:
-                dummyList=[]
-                for i0 in range(0,6):
-                    for i1 in range(0,6):
-                        for i2 in range(0,6):
-                            for i3 in range(0,6):
-                                opSecret=[i0,i1,i2,i3]
-                                if HowManyBc(guess[:],opSecret[:])==BC:
-                                    dummyList.append(opSecret)
-                List1=[item for item in optionList if item in dummyList]
-                optionList=List1[:]
-                nextGuess1=[]
-                item1Max=0
-                #L1=optionList[:]
-                #L2=allList[:]
-                L2=optionList[:]
-                L1=allList[:]
-                for item1 in L1:
-                    ListBC=[]
-                    for item2 in L2:
-                        ListBC.append(HowManyBc(item1[:],item2[:]))
-                    HMList=[0]*14
-                    for BC1 in ListBC:
-                        index=Adjustment(BC1)
-                        HMList[index]=HMList[index]+1
-                    #m=MinimumNozeros(HMList[:])
-                    m=len(L1)-max(HMList[:])
-                    if m>item1Max:
-                        item1Max=m
-                        nextGuess1=item1[:]
-                guess=nextGuess1[:]
-                BC=HowManyBc(guess[:],secret[:])
-                counter=counter+1
-        print(Secretcode)
-        print(counter)
 
 #functie die de simpele strategie volgt
 def simplestrat(Secretcode):
@@ -188,7 +81,8 @@ def simplestrat(Secretcode):
         #maak een gok met een willekeurige mogelijke optie
         guess = random.choice(currentoption)
         count += 1
-        feedback = pegstest(guess, Secretcode,algo_choice)
+        feedback = pegs(guess, Secretcode,algo_choice)
+
         print(f'guess: {guess} feedback:{feedback}')
 
         if feedback == [4,0]:
@@ -201,9 +95,7 @@ def simplestrat(Secretcode):
                 for i2 in range(0, 6):
                     for i3 in range(0, 6):
                         checksecret=[i0,i1,i2,i3]
-                        print(f'result: { pegstest(guess, checksecret, algo_choice)}, checksecret : {checksecret}, guess: {guess}')
-
-                        if pegstest(guess, checksecret, algo_choice)==feedback:
+                        if pegs(guess, checksecret, algo_choice)==feedback:
                             checklst.append(checksecret)
         currentoption =[item for item in currentoption if item in checklst]
 
